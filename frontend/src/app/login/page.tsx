6dcd7220b087adec49037c65f2bd4b285a8c2404
Akshay <akshay@thesileo.com>
2025-07-01 13:28:05 +0530

    latest

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -41,9 +41,10 @@ function LoginPage() {
   };
 
   return (
-    <div className="flex flex-col min-h-screen bg-gradient-to-r from-blue-100 to-purple-100">
+    <div className="flex flex-col min-h-screen bg-[url(/img/bg-login.jpg">
       <main className="flex-grow flex justify-center items-center px-4 py-10">
-        <div className="bg-white p-8 rounded-2xl shadow-xl w-full max-w-md">
+        <div className="w-1/2"><img src="" alt="" /></div>
+        <div className="bg-white p-8 rounded-2xl shadow-xl w-1/2">
           <h2 className="text-3xl font-bold text-center text-purple-600 mb-8">
             Login
           </h2>
